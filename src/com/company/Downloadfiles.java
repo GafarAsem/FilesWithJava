7adca9fac6ace63173bc17be953983b293e61b8b
gafar <gafarasem25@gmail.com>
2021-02-24 23:13:25 +0300

    buffred Reader

--- src/com/company/Downloadfiles.java
+++ src/com/company/Downloadfiles.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Downloadfiles {
 
+    public static void main(String[] args) throws IOException {
 
+        FileReader file=new FileReader("C:\\Users\\DELL\\IdeaProjects\\FilesWithJava\\src\\hello.txt");
+        BufferedReader br = new BufferedReader(file);
+        //read BufferedReader Object
+       for(Object r:br.lines().toArray()){
+           print(r);
+       }
+
+    }
+    public static void print(Object o){
+        System.out.println(o);
+    }
 
 
 }
