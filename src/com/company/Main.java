367d3904aacf9b2f2a752b405dfdba4b748e99c6
gafar <gafarasem25@gmail.com>
2021-02-24 23:01:15 +0300

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        //open file
         File file = new File("hello.txt");
         if(!file.exists()){
             file.createNewFile();
@@ -17,13 +18,22 @@ public class Main {
 
         FileWriter fileWriter=new FileWriter(file);
         fileWriter.append("hello mr gafar /n hello");
-        //close to write file
+//        //...........close to write file............
         fileWriter.flush();
         fileWriter.close();
 
          Scanner fileReader = new Scanner(file) ;
+//        //............print file text..............
          while (fileReader.hasNextLine())
          print(fileReader.nextLine());
+
+        //..............get Info file................
+        print(file.getPath());
+        print(file.getTotalSpace());
+        print(file.getCanonicalPath());
+
+
+
 
 
     }
